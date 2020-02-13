0807c9d58b7d3265da6ef3015d719c8c5792c0d0
Julius Wendt <julius.wendt@live.se>
2020-02-13 11:34:44 +0000

    setup connection, no msg exchange yet

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
-
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netdb.h>
 
 // Included to get the support library
 #include <calcLib.h>
@@ -9,6 +15,41 @@
 int main(int argc, char *argv[]){
   
   /* Do magic */
-  
+  if(argc < 3){
+    fprintf(stderr, "usage: %s <hostname> <port>", argv[0]);
+  }
 
+  initCalcLib();
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd == -1){
+    fprintf(stderr, "Failed to create socket\n");
+    return 1;
+  }
+
+  short port = atoi(argv[2]);
+  const char* paddr = argv[1];
+  hostent* serverEnt = gethostbyname(paddr);
+  if(serverEnt == NULL) {
+    fprintf(stderr, "No host : %s\n", paddr);
+    return 2;
+  }
+  sockaddr_in serverAddr;
+  socklen_t serverAddrlen = sizeof(serverAddr);
+  memset(&serverAddr, 0, serverAddrlen);
+  inet_pton(AF_INET, paddr, &serverAddr.sin_addr);
+  serverAddr.sin_port = htons(port);
+  serverAddr.sin_family = AF_INET;
+
+  int res = connect(sockfd, (sockaddr*)&serverAddr, serverAddrlen);
+  if(res != 0){
+    getsockname(sockfd, (sockaddr*)&serverAddr, &serverAddrlen);
+    char buffer[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, (sockaddr*)&serverAddr, buffer, sizeof(buffer));
+    fprintf(stderr, "Failed to connect to %s:%d\n", buffer, ntohs(serverAddr.sin_port));
+    return 3;
+  }
+
+  shutdown(sockfd, SHUT_RDWR);
+  return 0;
 }
--- servermain.cpp
+++ servermain.cpp
@@ -1,19 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 
 // Included to get the support library
 #include <calcLib.h>
 
 
-
 using namespace std;
 
+int bind_socket(int socket, short port, sockaddr_in& addr) {
+  socklen_t serverAddrlen = sizeof(addr);
+  memset(&addr, 0, serverAddrlen);
+  addr.sin_port = htons(port);
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  return bind(socket, (sockaddr*)&addr, serverAddrlen);
+}
+
+void getAddrStr(sockaddr_in& addr, char* str, size_t strlen) {
+  inet_ntop(AF_INET, &addr.sin_addr, str, strlen);
+}
 
 int main(int argc, char *argv[]){
-  
   /* Do more magic */
+  if(argc < 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    return 1;
+  }
+  initCalcLib();
   
+  short port = atoi(argv[1]);
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd == -1){
+    fprintf(stderr, "Failed to create socket\n");
+    return 2;
+  }
+  sockaddr_in serverAddr;
+  if(bind_socket(sockfd, port, serverAddr) == -1) {
+    fprintf(stderr, "Failed to bind to port\n");
+    return 3;
+  }
+
+  char name[INET_ADDRSTRLEN];
+  getAddrStr(serverAddr, name, sizeof(name));
+
+  int listerr = listen(sockfd, 5);
+  if(listerr == -1) {
+    fprintf(stderr, "Failed to listen\n");
+    return 4;
+  }
+
+  printf("Listening on %s:%d\n", name, ntohs(serverAddr.sin_port));
+
+  while(true) {
+    sockaddr_in clientAddr;
+    socklen_t clientlen;
+    int clientsock = accept(sockfd, (sockaddr*)&clientAddr, &clientlen);
+    if(clientsock == -1){
+      fprintf(stderr, "Failed to accept client\n");
+    }
+    else{
+      char cbuf[INET_ADDRSTRLEN];
+      getAddrStr(clientAddr, cbuf, sizeof(cbuf));
+      printf("%s connected\n", cbuf);
+    }
+    //Send protocoll
+
+    //Receive conformation
+
+    //Send operation
+
+    //Receive answer
+
+    //Check if correct
+
+    //Send OK if correct, ERROR otherwise
+
+  }
+
+  shutdown(sockfd, SHUT_RDWR);
+  return 0;  
 
 }
